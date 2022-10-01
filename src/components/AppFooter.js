eb0e4fb14bc1c6ad50ea0494102247270f041a5a
Joe Wang <zhouqiang.wang@hotmail.com>
2022-10-01 15:10:38 -0700

    Rebrand the AppFooter.

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -5,15 +5,15 @@ const AppFooter = () => {
   return (
     <CFooter>
       <div>
-        <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">
-          CoreUI
+        <a href="https://github.com/zhouqiangWang" target="_blank" rel="noopener noreferrer">
+          Audio2Video
         </a>
-        <span className="ms-1">&copy; 2022 creativeLabs.</span>
+        <span className="ms-1">&copy; 2022 SeenLab.</span>
       </div>
       <div className="ms-auto">
         <span className="me-1">Powered by</span>
-        <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">
-          CoreUI React Admin &amp; Dashboard Template
+        <a href="https://github.com/zhouqiangWang" target="_blank" rel="noopener noreferrer">
+          Video your podcast.
         </a>
       </div>
     </CFooter>
